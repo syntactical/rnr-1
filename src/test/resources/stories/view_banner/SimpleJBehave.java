71fcb09d63d7dc10ca1b42c5a21d648764c4c531
Svetlana Ivanova <sivanova@thoughtworks.com>
2013-09-10 16:24:24 -0500

    updates to config file

--- src/test/resources/stories/view_banner/SimpleJBehave.java
+++ src/test/resources/stories/view_banner/SimpleJBehave.java
@@ -10,10 +10,47 @@ public class SimpleJBehave extends JUnitStories {
     public SimpleJBehave() {
         super();
         this.configuredEmbedder().candidateSteps().add(new NavigationSteps());
+
+        configuration.useFailureStrategy(new RethrowingFailure());
+        configuration.useKeywords(new LocalizedKeywords(Locale.ENGLISH));
+        configuration.usePathCalculator(new AbsolutePathCalculator());
+        configuration.useParameterControls(new ParameterControls());
+        configuration.useParameterConverters(new ParameterConverters());
+        configuration.useParanamer(new NullParanamer());
+        configuration.usePendingStepStrategy(new PassingUponPendingStep());
+        configuration.useStepCollector(new MarkUnmatchedStepsAsPending());
+        configuration.useStepdocReporter(new PrintStreamStepdocReporter());
+        configuration.useStepFinder(new StepFinder());
+        configuration.useStepMonitor(new SilentStepMonitor());
+        configuration
+                .useStepPatternParser(new RegexPrefixCapturingPatternParser());
+        configuration.useStoryControls(new StoryControls());
+        configuration.useStoryLoader(new LoadFromClasspath());
+        configuration.useStoryParser(new RegexStoryParser(configuration
+                .keywords()));
+        configuration.useStoryPathResolver(new UnderscoredCamelCaseResolver());
+        configuration.useStoryReporterBuilder(new StoryReporterBuilder());
+        configuration.useViewGenerator(new FreemarkerViewGenerator());
+
+        EmbedderControls embedderControls = configuredEmbedder()
+                .embedderControls();
+        embedderControls.doBatch(false);
+        embedderControls.doGenerateViewAfterStories(true);
+        embedderControls.doIgnoreFailureInStories(false);
+        embedderControls.doIgnoreFailureInView(false);
+        embedderControls.doSkip(false);
+        embedderControls.doVerboseFailures(false);
+        embedderControls.doVerboseFiltering(false);
+        embedderControls.useStoryTimeoutInSecs(300);
+        embedderControls.useThreads(1);
     }
 
-    @Override
-    protected List<String> storyPaths() {
-        return Arrays.asList("de/codecentric/simplejbehave/Math.story");
+    private static Embedder embedder = new Embedder();
+    private static List<String> storyPaths = Arrays
+            .asList("de/codecentric/simplejbehave/banner.story");
+
+    public static void main(String[] args) {
+        embedder.candidateSteps().add(new ExampleSteps());
+        embedder.runStoriesAsPaths(storyPaths);
     }
 }
