4d3536776639107f5a6cc220ebdcc262969f8ce0
Michael Pai <mpai@thoughtworks.com>
2013-12-10 14:18:55 -0600

    [michael and greg] remove vacay.jsp and unused js functions

--- src/main/webapp/scripts/js/rnr.js
+++ src/main/webapp/scripts/js/rnr.js
@@ -17,31 +17,6 @@ $(function () {
     });
 });
 
-//$(function () {
-//    function getTodaysDate() {
-//        var today = new Date();
-//        var dd = today.getDate();
-//        var mm = today.getMonth() + 1; //January is 0!
-//        var yyyy = today.getFullYear();
-//
-//        if (dd < 10) {
-//            dd = '0' + dd
-//        }
-//        if (mm < 10) {
-//            mm = '0' + mm
-//        }
-//        today = mm + '/' + dd + '/' + yyyy;
-//
-//        return today;
-//    }
-//
-//    document.getElementById('end-date-picker').value=getTodaysDate()
-//})
-
-//$(function setDefaultInitialAccrualRate(){
-//    document.getElementById('accrual-rate').value="10";
-//})
-
 function showFirstSalesForcePage() {
     document.getElementById('sales-force-help-box-1').style.zIndex = "-1";
     document.getElementById('sales-force-help-box-2').style.zIndex = "-2";
